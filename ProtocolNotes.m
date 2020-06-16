5f41b76b6519dbb20196a0be01b68e9a47e221a2
Jennifer Bussell <jbussell@gmail.com>
2020-06-16 16:34:16 -0400

    notes

---
+++ ProtocolNotes.m
@@ -0,0 +1,26 @@
+%{
+Params
+
+ChooseLeft
+ChooseRight
+StimulusOutput
+CenterOdor
+RewardLeft
+RewardRight
+RightSideOdor
+LeftSideOdor
+OutcomeStateLeft
+OutcomeStateRight
+LeftRewardDrops
+RightRewardDrops
+
+GlobalTimer1 = Odor delay
+Condition7 = when it expires
+
+For time of max drops:
+GlobalCounter2 = globaltimer2 end, maxdrops
+
+GlobalTimer 3 = valve time for reward drops
+global counter 3 = counts those for right drops
+
+%}
