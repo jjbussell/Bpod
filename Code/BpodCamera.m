3fcc3c673a02359031ca2413a4d5f776139b8fb3
Jennifer Bussell <jbussell@gmail.com>
2020-08-04 17:26:21 -0400

    camera control script

---
+++ Code/BpodCamera.m
@@ -0,0 +1,108 @@
+info=imaqhwinfo('winvideo');
+dev_info = info.DeviceInfo(1);
+dev_info.SupportedFormats;
+
+vid = videoinput('winvideo',1,'MJPG_960x720');
+src.AcquisitionFrameRateEnable = 'True';
+src.AcquisitionFrameRateAbs = 30;
+vid.FramesPerTrigger = Inf;
+triggerconfig(vid, 'manual');
+logfile = VideoWriter('testvid.avi','Motion JPEG AVI');
+set(logfile,'FrameRate',30);
+vid.DiskLogger = logfile;
+set(vid,'LoggingMode','disk');
+
+preview(vid);
+
+start(vid);
+trigger(vid);
+
+pause(30);
+
+stop(vid);
+
+flushdata(vid);
+delete(vid);
+clear vid;
+
+% generatePreviewInGui;
+% figSize = get(gca,'Position');
+% figWidth = figSize(3);
+% figHeight = figSize(4);
+% vidRes = get(vid, 'VideoResolution');
+% imWidth = vidRes(1);
+% imHeight = vidRes(2);
+% % set(gca,'unit','pixels',...
+% %         'position',[ (335+(figWidth - imWidth)/2)...
+% %                      (330+(figHeight - imHeight)/2)...
+% %                        imWidth imHeight ]);
+% set(gca,'unit','pixels',...
+%     'position',[ (1100+(figWidth - imWidth)/2)...
+%     (200+(figHeight - imHeight)/2)...
+%     imWidth imHeight ]);
+% 
+% function generatePreviewInGui
+% global vid
+% 
+% % Create the text label for the timestamp
+% %hTextLabel = uicontrol('style','text','String','Timestamp', ...
+% %    'Units','normalized','fontsize',16,...
+% %    'Position',[0.05 .04 .15 .08]); %[0.85 -.04 .15 .08]);
+% 
+% % Create the image object in which you want to
+% % display the video preview data.
+% vidRes = get(vid, 'VideoResolution');
+% imWidth = vidRes(1);
+% imHeight = vidRes(2);
+% nBands = get(vid, 'NumberOfBands');
+% hImage = image( zeros(imHeight, imWidth, nBands) );
+%           
+% % Set up the update preview window function.
+% %setappdata(hImage,'UpdatePreviewWindowFcn',@mypreview_fcn);
+% 
+% % Make handle to text label available to update function.
+% %setappdata(hImage,'HandleToTimestampLabel',hTextLabel);
+% 
+% preview(vid, hImage);
+% 
+% function startVid
+% global trialParams vid fileStruct trialCounter logfile vidCounter
+% 
+% fileStruct.fileName = strcat(fileStruct.folder,fileStruct.genotype,'_exp_',fileStruct.expNum,'_',char(fileStruct.thisTrialState),'_vid_',num2str(vidCounter)) ;
+% %logfile = VideoWriter(fileStruct.fileName, 'Motion JPEG AVI');
+% %set(logfile,'FrameRate',25);
+% %vid.DiskLogger = logfile;
+% 
+% % vid = videoinput('gige', 1, 'Mono8');
+% % src = getselectedsource(vid);
+% % src.AcquisitionFrameRateEnable = 'True';
+% % src.AcquisitionFrameRateAbs = 25;
+% % vid.FramesPerTrigger = 100;
+% % triggerconfig(vid, 'manual');
+% logfile = VideoWriter(fileStruct.fileName, 'Motion JPEG AVI'); %'Uncompressed AVI'); %
+% set(logfile,'FrameRate',25);
+% vid.DiskLogger = logfile;
+% set(vid,'LoggingMode','disk');
+% %vid.ROIPosition = [593 1011 596 114];
+% start(vid);
+% trigger(vid);
+% trialCounter = 0; %number of manually started trials
+% fileStruct.trueCStimes = [];
+% fileStruct.trueUStimes = [];
+% fileStruct.trueStartTimes = [];
+% trialParams.presentedOdor = [];
+% trialParams.isAversiveTrial = [];
+% 
+% function stopVid
+% global trialParams vid trialCounter
+% 
+% stop(vid)
+% trialParams.numTrials = trialCounter;
+% %[fileStruct.flyvid, fileStruct.timeStamp] = getdata(vid);
+% %annotateVideo;
+% saveFile;
+% flushdata(vid);
+% delete(vid);
+% clear vid;
+% makeVidObject;
+% generatePreviewInGui;
