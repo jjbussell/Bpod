f0fe11d7fbe31e306ce5c1b88ee7f19885004856
Jennifer Bussell <jbussell@gmail.com>
2020-08-11 17:05:08 -0400

    odortest all odors work, still need latch valves! and cycle through odors

--- Protocols/OdorTest/OdorTest.m
+++ Protocols/OdorTest/OdorTest.m
@@ -17,7 +17,7 @@ if isempty(fieldnames(S))  % If settings file was an empty struct, populate stru
     S.GUI.OdorTime = 3;
     S.GUI.OdorInterval = 4;
     S.GUI.Port = 0; %0 = center, 1 = left, 2 = right
-    S.GUI.OdorID = 1;
+    S.GUI.OdorID = 1; % 0 = odor 1
 end
 
 %% Initialize plots
