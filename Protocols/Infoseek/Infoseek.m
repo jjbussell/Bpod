e9f135bceb294dfc59f42bf22c399dca43d32dbb
Jennifer Bussell <jbussell@gmail.com>
2020-09-30 17:29:14 -0400

    moving trial types to a function

--- Protocols/Infoseek/Infoseek.m
+++ Protocols/Infoseek/Infoseek.m
@@ -68,191 +68,13 @@ infoSide = S.GUI.InfoSide; % 0 = info on left
 
 %% Set up trial types and rewards
 
-S = SetTrialTypes(S);
-
-%% Define trial choice types
-
-MaxTrials = S.GUI.SessionTrials;
-
-typesAvailable = S.GUI.TrialTypes;
-
-blockSize = 12;
-typeBlockSize = 8;
-choicePercent = 0; infoPercent = 0; randPercent = 0;
-
-switch typesAvailable 
-    case 1
-        choicePercent = 1; infoPercent = 0; randPercent = 0;
-    case 2
-        choicePercent = 0; infoPercent = 1; randPercent = 0;
-    case 3
-        choicePercent = 0; infoPercent = 0; randPercent = 1;
-    case 4
-        choicePercent = 0; infoPercent = 0.5; randPercent = 0.5;
-    case 5
-        choicePercent = 0.334; infoPercent = 0.334; randPercent = 0.334;        
-    case 6
-        choicePercent = 0; infoPercent = 0.85; randPercent = 0.15;
-    case 7
-        choicePercent = 0.5; infoPercent = 0.5; randPercent = 0;
-    case 8
-        choicePercent = 0.5; infoPercent = 0; randPercent = 0.5;        
-end
-
-% set trial type arrays based on TrialTypes
-choiceBlockSize = round(choicePercent * blockSize);
-infoBlockSize = round(infoPercent * blockSize);
-randBlockSize = round(randPercent * blockSize);
-
-blockToShuffle = zeros(blockSize,1);
-
-if choiceBlockSize > 0
-    blockToShuffle(1:choiceBlockSize) = 1;
-end
-
-if infoBlockSize > 0
-    if choiceBlockSize > 0
-        blockToShuffle(choiceBlockSize+1:choiceBlockSize + infoBlockSize) = 2;
-    else
-        blockToShuffle(1:infoBlockSize) = 2;
-    end
-end
-if randBlockSize > 0
-    blockToShuffle(choiceBlockSize + infoBlockSize + 1:end) = 3;
-end
-
-blocks = ceil(MaxTrials/blockSize);
-TrialTypes = zeros(blocks*blockSize,1);
-
-block=blockToShuffle;
-
-for n = 1:blocks
-    for m = 1:blockSize
-        i = randi(blockSize);
-        temp = block(m);
-        block(m) = block(i);
-        block(i) = temp;
-    end
-    if n == 1
-       TrialTypes(1:blockSize) = block; 
-    else
-        TrialTypes((n-1)*blockSize+1:n*blockSize) = block;
-    end
-end
-
-% trial choiceTypes
-% TrialTypes = [2; 2; 3; 3; 2; 2; 3; 3; TrialTypes];
-TrialTypes=TrialTypes(1:MaxTrials);
-
-PlotOutcomes = NaN(1,MaxTrials);
-Outcomes = NaN(1,MaxTrials);
+S = SetTrialTypes(S); % Sets S.TrialTypes, S.RewardTypes, S.RandOdorTypes
 
 BpodSystem.Data.TrialTypes = []; % The trial type of each trial completed will be added here.
 BpodSystem.Data.Outcomes = [];
 
-%% SET REWARD BLOCKS
-
-infoBigCount = round(S.GUI.InfoRewardProb*typeBlockSize);
-randBigCount = round(S.GUI.RandRewardProb*typeBlockSize);
-
-infoBlockShuffle = zeros(typeBlockSize,1);
-randBlockShuffle = zeros(typeBlockSize,1);
-
-infoBlockShuffle(1:infoBigCount) = 1;
-randBlockShuffle(1:randBigCount) = 1;
-
-typeBlockCount = ceil(MaxTrials/typeBlockSize);
-RewardTypes = zeros(typeBlockCount*typeBlockSize,4);
-RandOdorTypes = zeros(typeBlockCount*typeBlockSize,1);
-
-infoBlock = infoBlockShuffle;
-randBlock = randBlockShuffle;
-randOdorBlock = randBlockShuffle;
-
-% info choice
-for n = 1:typeBlockCount
-    for m = 1:typeBlockSize
-        i = randi(typeBlockSize);
-        temp = infoBlock(m);
-        infoBlock(m) = infoBlock(i);
-        infoBlock(i) = temp;
-    end
-    if n == 1
-       RewardTypes(1:typeBlockSize,1) = infoBlock'; 
-    else
-        RewardTypes((n-1)*typeBlockSize+1:n*typeBlockSize,1) = infoBlock';
-    end
-end
-
-% rand choice
-for n = 1:typeBlockCount
-    for m = 1:typeBlockSize
-        i = randi(typeBlockSize);
-        temp = randBlock(m);
-        randBlock(m) = randBlock(i);
-        randBlock(i) = temp;
-    end
-    if n == 1
-       RewardTypes(1:typeBlockSize,2) = randBlock'; 
-    else
-        RewardTypes((n-1)*typeBlockSize+1:n*typeBlockSize,2) = randBlock';
-    end
-end
-
-% info forced
-for n = 1:typeBlockCount
-    for m = 1:typeBlockSize
-        i = randi(typeBlockSize);
-        temp = infoBlock(m);
-        infoBlock(m) = infoBlock(i);
-        infoBlock(i) = temp;
-    end
-    if n == 1
-       RewardTypes(1:typeBlockSize,3) = infoBlock'; 
-    else
-        RewardTypes((n-1)*typeBlockSize+1:n*typeBlockSize,3) = infoBlock';
-    end
-end
-
-% rand forced
-for n = 1:typeBlockCount
-    for m = 1:typeBlockSize
-        i = randi(typeBlockSize);
-        temp = randBlock(m);
-        randBlock(m) = randBlock(i);
-        randBlock(i) = temp;
-    end
-    if n == 1
-       RewardTypes(1:typeBlockSize,4) = randBlock'; 
-    else
-        RewardTypes((n-1)*typeBlockSize+1:n*typeBlockSize,4) = randBlock';
-    end
-end
-
-% rand odors
-for n = 1:typeBlockCount
-    for m = 1:typeBlockSize
-        i = randi(typeBlockSize);
-        temp = randOdorBlock(m);
-        randOdorBlock(m) = randOdorBlock(i);
-        randOdorBlock(i) = temp;
-    end
-    if n == 1
-       RandOdorTypes(1:typeBlockSize) = randOdorBlock'; 
-    else
-        RandOdorTypes((n-1)*typeBlockSize+1:n*typeBlockSize) = randOdorBlock';
-    end
-end
-
-% Trial types (rewards) to pull from
-RewardTypes = RewardTypes(1:MaxTrials,:);
-
-% Rand Odors to pull from
-% RandOdorTypes = repmat(RandOdorTypes,1,4);
-RandOdorTypes = RandOdorTypes(1:MaxTrials);
-
-BpodSystem.Data.OrigTrialTypes = TrialTypes;
-BpodSystem.Data.RewardTypes = RewardTypes;
+BpodSystem.Data.OrigTrialTypes = S.TrialTypes;
+BpodSystem.Data.OrigRewardTypes = S.RewardTypes;
 
 %% Initialize plots
 
@@ -783,6 +605,190 @@ end
 
 function updatedtypes = UpdateTrialTypes(i,trialType,TrialTypes)
     updatedtypes = [TrialTypes(1:i); TrialTypes(i); TrialTypes(i+1:end-1)];
+end
+
+function settingsStructUpdated = SetTrialTypes(settingsStruct)
+
+    %% Define trial choice types
+
+    maxTrials = S.GUI.SessionTrials;
+
+    typesAvailable = S.GUI.TrialTypes;
+
+    blockSize = 12;
+    typeBlockSize = 8;
+    choicePercent = 0; infoPercent = 0; randPercent = 0;
+
+    switch typesAvailable 
+        case 1
+            choicePercent = 1; infoPercent = 0; randPercent = 0;
+        case 2
+            choicePercent = 0; infoPercent = 1; randPercent = 0;
+        case 3
+            choicePercent = 0; infoPercent = 0; randPercent = 1;
+        case 4
+            choicePercent = 0; infoPercent = 0.5; randPercent = 0.5;
+        case 5
+            choicePercent = 0.334; infoPercent = 0.334; randPercent = 0.334;        
+        case 6
+            choicePercent = 0; infoPercent = 0.85; randPercent = 0.15;
+        case 7
+            choicePercent = 0.5; infoPercent = 0.5; randPercent = 0;
+        case 8
+            choicePercent = 0.5; infoPercent = 0; randPercent = 0.5;        
+    end
+
+    % set trial type arrays based on TrialTypes
+    choiceBlockSize = round(choicePercent * blockSize);
+    infoBlockSize = round(infoPercent * blockSize);
+    randBlockSize = round(randPercent * blockSize);
+
+    blockToShuffle = zeros(blockSize,1);
+
+    if choiceBlockSize > 0
+        blockToShuffle(1:choiceBlockSize) = 1;
+    end
+
+    if infoBlockSize > 0
+        if choiceBlockSize > 0
+            blockToShuffle(choiceBlockSize+1:choiceBlockSize + infoBlockSize) = 2;
+        else
+            blockToShuffle(1:infoBlockSize) = 2;
+        end
+    end
+    if randBlockSize > 0
+        blockToShuffle(choiceBlockSize + infoBlockSize + 1:end) = 3;
+    end
+
+    blocks = ceil(maxTrials/blockSize);
+    TrialTypes = zeros(blocks*blockSize,1);
+
+    block=blockToShuffle;
+
+    for n = 1:blocks
+        for m = 1:blockSize
+            i = randi(blockSize);
+            temp = block(m);
+            block(m) = block(i);
+            block(i) = temp;
+        end
+        if n == 1
+           TrialTypes(1:blockSize) = block; 
+        else
+            TrialTypes((n-1)*blockSize+1:n*blockSize) = block;
+        end
+    end
+
+    % TrialTypes = [2; 2; 3; 3; 2; 2; 3; 3; TrialTypes];
+    TrialTypes=TrialTypes(1:maxTrials);
+    
+    S.TrialTypes = TrialTypes;
+
+    
+    %% SET REWARD BLOCKS
+
+    infoBigCount = round(S.GUI.InfoRewardProb*typeBlockSize);
+    randBigCount = round(S.GUI.RandRewardProb*typeBlockSize);
+
+    infoBlockShuffle = zeros(typeBlockSize,1);
+    randBlockShuffle = zeros(typeBlockSize,1);
+
+    infoBlockShuffle(1:infoBigCount) = 1;
+    randBlockShuffle(1:randBigCount) = 1;
+
+    typeBlockCount = ceil(MaxTrials/typeBlockSize);
+    RewardTypes = zeros(typeBlockCount*typeBlockSize,4);
+    RandOdorTypes = zeros(typeBlockCount*typeBlockSize,1);
+
+    infoBlock = infoBlockShuffle;
+    randBlock = randBlockShuffle;
+    randOdorBlock = randBlockShuffle;
+
+    % info choice
+    for n = 1:typeBlockCount
+        for m = 1:typeBlockSize
+            i = randi(typeBlockSize);
+            temp = infoBlock(m);
+            infoBlock(m) = infoBlock(i);
+            infoBlock(i) = temp;
+        end
+        if n == 1
+           RewardTypes(1:typeBlockSize,1) = infoBlock'; 
+        else
+            RewardTypes((n-1)*typeBlockSize+1:n*typeBlockSize,1) = infoBlock';
+        end
+    end
+
+    % rand choice
+    for n = 1:typeBlockCount
+        for m = 1:typeBlockSize
+            i = randi(typeBlockSize);
+            temp = randBlock(m);
+            randBlock(m) = randBlock(i);
+            randBlock(i) = temp;
+        end
+        if n == 1
+           RewardTypes(1:typeBlockSize,2) = randBlock'; 
+        else
+            RewardTypes((n-1)*typeBlockSize+1:n*typeBlockSize,2) = randBlock';
+        end
+    end
+
+    % info forced
+    for n = 1:typeBlockCount
+        for m = 1:typeBlockSize
+            i = randi(typeBlockSize);
+            temp = infoBlock(m);
+            infoBlock(m) = infoBlock(i);
+            infoBlock(i) = temp;
+        end
+        if n == 1
+           RewardTypes(1:typeBlockSize,3) = infoBlock'; 
+        else
+            RewardTypes((n-1)*typeBlockSize+1:n*typeBlockSize,3) = infoBlock';
+        end
+    end
+
+    % rand forced
+    for n = 1:typeBlockCount
+        for m = 1:typeBlockSize
+            i = randi(typeBlockSize);
+            temp = randBlock(m);
+            randBlock(m) = randBlock(i);
+            randBlock(i) = temp;
+        end
+        if n == 1
+           RewardTypes(1:typeBlockSize,4) = randBlock'; 
+        else
+            RewardTypes((n-1)*typeBlockSize+1:n*typeBlockSize,4) = randBlock';
+        end
+    end
+
+    % rand odors
+    for n = 1:typeBlockCount
+        for m = 1:typeBlockSize
+            i = randi(typeBlockSize);
+            temp = randOdorBlock(m);
+            randOdorBlock(m) = randOdorBlock(i);
+            randOdorBlock(i) = temp;
+        end
+        if n == 1
+           RandOdorTypes(1:typeBlockSize) = randOdorBlock'; 
+        else
+            RandOdorTypes((n-1)*typeBlockSize+1:n*typeBlockSize) = randOdorBlock';
+        end
+    end
+
+    % Trial types (rewards) to pull from
+    RewardTypes = RewardTypes(1:MaxTrials,:);
+    S.RewardTypes = RewardTypes;
+
+    % Rand Odors to pull from
+    % RandOdorTypes = repmat(RandOdorTypes,1,4);
+    RandOdorTypes = RandOdorTypes(1:MaxTrials);
+    
+    S.RandOdorTypes = RandOdorTypes;
+
 end
 
 %% ODOR CONTROL
