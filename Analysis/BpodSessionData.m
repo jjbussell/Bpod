da510bfd0cc9612cd597f4d8e623e8c74bf6d679
Jennifer Bussell <jbussell@gmail.com>
2020-10-19 14:20:28 -0400

    bpod session data works

--- Analysis/BpodSessionData.m
+++ Analysis/BpodSessionData.m
@@ -43,7 +43,10 @@ loadData = 0;
 
 if loadData == 1
     fname = 'infoSeekBpodData.mat';
-    load(fname); % opens structure "a" with previous data, if available    
+    load(fname); % opens structure "a" with previous data, if available
+    for fn = 1:a.numFiles
+        names{fn} = a.files(fn).name; 
+    end
 end
 
 %% LOAD NEW DATA
@@ -55,52 +58,91 @@ numFiles = size(files,1);
 
 f = 3;
 
-    b = struct;
 for f = 1:numFiles
 
+    clearvars -except a names pathname files numFiles f loadData;
 
     filename = files(f).name;
 
-    fname = fullfile(pathname,filename);
-    breaks = strfind(filename,'_');
+    filepath = fullfile(pathname,filename);
     
-    b.filename(f,1) = cellstr(filename);
-    b.mouse(f,1) = cellstr(filename(1:breaks(1)-1));
-    b.protocol(f,1) = cellstr(filename(breaks(1)+1:breaks(2)-1));
-    b.day(f,1) = cellstr(filename(breaks(2)+1:breaks(3)-1));
-    b.startTime(f,1) = cellstr(filename(breaks(3)+1:strfind(filename,'.')-1));
+    
+    % need to check for duplicates!!!
+    
+%     breaks = strfind(filename,'_');
+    
+%     b.filename(f,1) = cellstr(filename);
+%     b.mouse(f,1) = cellstr(filename(1:breaks(1)-1));
+%     b.protocol(f,1) = cellstr(filename(breaks(1)+1:breaks(2)-1));
+%     b.day(f,1) = cellstr(filename(breaks(2)+1:breaks(3)-1));
+%     b.startTime(f,1) = cellstr(filename(breaks(3)+1:strfind(filename,'.')-1));    
     
     % Pull raw data from matfile
-    load(fname);    
-    b.data(f,1) = SessionData;
+    load(filepath);    
+%     b.data{f,1} = SessionData;
     
-    % Break down session data into per-file variables
-    sessionVariables = {'nTrials','SettingsFile','TrialSettings','Notes',...
-        'TrialCounts','EventNames','TrialTypes','Outcomes','TrialStartTimestamp','TrialEndTimestamp','RawEvents'};
+%     % Break down session data into per-file variables
+%     sessionVariables = {'nTrials','SettingsFile','TrialSettings','Notes',...
+%         'TrialCounts','EventNames','TrialTypes','Outcomes','TrialStartTimestamp','TrialEndTimestamp','RawEvents'};
+%     
+%     for i = 1:numel(sessionVariables)
+%         name = sessionVariables{i};
+%        if isfield(SessionData,name)
+%           b.(name){f,1} = SessionData.(name);
+%        else
+%           b.(name){f,1} = []; 
+%        end
+%     end
     
-    for i = 1:numel(sessionVariables)
-        name = sessionVariables{i};
-       if isfield(SessionData,name)
-          b.(name){f,1} = SessionData.(name); 
-       end
-    end
+    % Session-level data    
+    session(f).name = filename;
+    session(f).settings = SessionData.SettingsFile.GUI;
+    session(f).eventNames = SessionData.EventNames;
+    session(f).nTrials = SessionData.nTrials;
     
-end
-    
-    if exist('a','var') == 0
 
+    
+    % Trial-level data
+    for t = 1:SessionData.nTrials
+       b.file(t,1) = f;  
+    end
+    b.trialSettings = [SessionData.TrialSettings(:)];
+%     b.trialSettings = [settings{:}]';
+    b.trialType = SessionData.TrialTypes';
+    b.startTime = SessionData.TrialStartTimestamp';
+    b.endTime = SessionData.TrialEndTimestamp';
+    b.outcome = SessionData.Outcomes';
+    trialData = [SessionData.RawEvents(:).Trial];
+    b.trialData = [trialData{:}]'; 
+    b.States = [b.trialData(:)];
+    
+    % Add this file's data to struct 'a'
+    if exist('a','var') == 0
         a = b;
         
     else
-       a.filename = [a.filename; b.filename];
-       a.mouse = [a.mouse; b.mouse];
-       a.protocol = [a.protocol; b.protocol];
-       a.day = [a.day; b.day];
+       a.file = [a.file; b.file];
+       a.trialSettings = [a.trialSettings; b.trialSettings];
+       a.trialType = [a.trialType; b.trialType];
        a.startTime = [a.startTime; b.startTime];
-       a.data = [a.data; b.data];
-    end
+       a.endTime = [a.endTime; b.endTime];
+       a.outcome = [a.outcome; b.outcome];
+       a.trialData = [a.trialData; b.trialData];
+    end    
     
-% end
+end % end for each file
+
+if isfield(a,'files') == 0
+   a.files = session;
+else       
+    a.files = [a.files; session];
+end
+
+save('infoSeekBpodData.mat','a');
+% uisave({'a'},'infoSeekFSMData.mat');
+
+save(['infoSeekFSMBpodData' datestr(now,'yyyymmdd')],'a');
+
 
 %%
 
