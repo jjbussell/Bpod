3e0b1c7e7163d8e168cfce19291bef8620a5d0da
Bussell Lab <busselllab@gmail.com>
2020-10-28 13:04:57 -0400

    adding valves 4

--- Protocols/OdorTestNewOlf/OdorTestNewOlf.m
+++ Protocols/OdorTestNewOlf/OdorTestNewOlf.m
@@ -18,15 +18,16 @@ if isempty(fieldnames(S))  % If settings file was an empty struct, populate stru
     S.GUI.OdorInterval = 10;
     S.GUI.OdorHeadstart = 4;
     S.GUI.Port = 1; %0 = center, 1 = left, 2 = right
-    S.GUI.OdorID = 1; % 0 = odor 1
+    S.GUI.OdorID = 0; % 0 = odor 1
 end
 
 
 %% LOAD SERIAL MESSAGES
 
-LoadSerialMessages('ValveModule1',{[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8]});
-LoadSerialMessages('ValveModule2',{[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8]});
-LoadSerialMessages('ValveModule3',{[1,2],[3,4],[5,6]});
+LoadSerialMessages('ValveModule1',{[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8]}); % switch control and odor 1-7, valves before
+LoadSerialMessages('ValveModule2',{[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8]}); % switch control and odor 1-7, valves after
+LoadSerialMessages('ValveModule3',{[1,2],[3,4],[5,6]}); % final valves switch control and odor, left, center, right
+LoadSerialMessages('ValveModule4',{[1,2],[3,2]}); % turn on right, turn on left
 
 %% Initialize plots
 
@@ -107,9 +108,22 @@ end
 % end
 
 function Actions = PreloadOdor(odorID)
-    cmd1 = {'ValveModule1',odorID};
-    cmd2 = {'ValveModule2',odorID}; 
-    Actions = [cmd1,cmd2];
+    switch port
+        case 0            
+            cmd1 = {'ValveModule1',odorID};
+            cmd2 = {'ValveModule2',odorID}; 
+            Actions = [cmd1,cmd2];
+        case 1
+            cmd1 = {'ValveModule1',odorID};
+            cmd2 = {'ValveModule2',odorID};
+            cmd3 = {'ValveModule4',1};
+            Actions = [cmd1,cmd2,cmd3];
+        case 2
+            cmd1 = {'ValveModule1',odorID};
+            cmd2 = {'ValveModule2',odorID};
+            cmd3 = {'ValveModule4',2};
+            Actions = [cmd1,cmd2,cmd3];            
+    end
 end
 
 function Actions = PresentOdor(port)
