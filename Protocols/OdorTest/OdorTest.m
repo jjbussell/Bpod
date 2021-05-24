c82f22c9ce6490fd82314e12f61205044078e1a1
Bussell Lab <busselllab@gmail.com>
2021-05-24 10:08:31 -0400

    not sure with doors

--- Protocols/OdorTest/OdorTest.m
+++ Protocols/OdorTest/OdorTest.m
@@ -16,7 +16,7 @@ if isempty(fieldnames(S))  % If settings file was an empty struct, populate stru
     S.GUI.SessionTrials = 1000;
     S.GUI.OdorTime = 1;
     S.GUI.OdorInterval = 1;
-    S.GUI.Port = 0; %0 = center, 1 = left, 2 = right
+    S.GUI.Port = 1; %0 = center, 1 = left, 2 = right
     S.GUI.OdorID = 0; % 0 = odor 1
 end
 
